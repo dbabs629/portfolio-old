66a9085a355610051cc2a002b2e0dc195704518a
Daniel Babin <daniel.l.babin@gmail.com>
2021-05-19 22:01:28 -0400

    updated projects section, nav, about section and added links to each button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,56 +1,42 @@
-let menuButton = document.getElementsByClassName("nav-menu-collapse");
-let navModal = document.getElementsByClassName("nav-modal");
-let navMenu = document.getElementsByClassName("nav-menu");
-let closeModal = document.getElementsByClassName("close-btn-wrapper");
+let navList = document.getElementsByClassName("nav-link-list");
+let projectContent = document.getElementsByClassName("project-section");
+let aboutContent = document.getElementsByClassName("about-section");
+let projectTab = document.getElementsByClassName("project-tab")[0];
+let aboutTab = document.getElementsByClassName("about-tab")[0];
+let contactTab = document.getElementsByClassName("contact-tab")[0];
+console.log(aboutTab.classList[0]);
 
-// menuButton[0].addEventListener("click", function(event){
-//     console.log(event);
-//     if (navModal[0].style.display == "none" || navModal[0].style.display == ""){
-//         navModal[0].style.display = "flex";
-//         menuButton[0].style.display = "none";
-//     } else {
-//         navModal[0].style.display ="none";
-//         menuButton[0].style.display = "block";
-//     }
-// });
-
-// closeModal[0].addEventListener("click", function(event){
-//     console.log(event);
-//     if (navModal[0].style.display == "flex" || navModal[0].style.display == ""){
-//         navModal[0].style.display = "none";
-//         menuButton[0].style.display = "block";
-//     } else {
-//         navModal[0].style.display ="block";
-//         menuButton[0].style.display = "none";
-//     }
-// });
-
-// window.addEventListener("scroll", function(){
-//     let menuScroll = Math.round(this.scrollY);
-//     console.log(menuScroll);
-//     if (menuScroll >= 50){
-//         menuButton[0].style.display = "block";
-//     } else if (menuScroll < 100){
-//         menuButton[0].style.display = "none";
-//     } else {
-//         console.log("error");
-//     }
-// });
-
-let txtDaniel = document.getElementsByClassName("animation-daniel");
-let txtBabin = document.getElementsByClassName("animation-babin");
-let scrollPos;
-
-document.addEventListener('scroll', (e) => {
-    scrollPos = e.path[1].window.scrollY;
-    if (scrollPos >= 100 && scrollPos <= 199.99){
-        animate
-    }
-    console.log(scrollPos);
+navList.addEventListener('click', function(e) {
+    
 });
 
+// projectTab.addEventListener('click', function() {
+//     aboutTab.classList.toggle("active");
+//     contactTab.classList.toggle("active");
+//     projectTab.classList.toggle("active");
+// });
 
+// aboutTab.addEventListener('click', function() {
+//     projectTab.classList.toggle("active");
+//     contactTab.classList.toggle("active");
+//     aboutTab.classList.toggle("active");
+// });
 
+// contactTab.addEventListener('click', function() {
+//     projectTab.classList.toggle("active");
+//     aboutTab.classList.toggle("active");
+//     contactTab.classList.toggle("active");
+// });
 
+// let txtDaniel = document.getElementsByClassName("animation-daniel");
+// let txtBabin = document.getElementsByClassName("animation-babin");
+// let scrollPos;
 
-// console.log(scrollPos);
+// document.addEventListener('scroll', (e) => {
+//     scrollPos = e.path[1].window.scrollY;
+//     if (scrollPos >= 100 && scrollPos <= 199.99){
+//         animate
+//     }
+//     console.log(scrollPos);
+// });
+
